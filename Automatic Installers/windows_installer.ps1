8a1773846f82612168584b22d4147ec01ac03b74
Henry G <henrygillett@duck.com>
2025-03-26 15:48:50 +0000

    Delete Automatic Installers directory

--- Automatic Installers/windows_installer.ps1
+++
@@ -1,9 +0,0 @@
-# Installs python-vlc
-pip install python-vlc
-
-# Clones the repository
-git clone https://github.com/Starman7312/internet_radio
-
-# Navigates to the folder and runs the Python script
-Set-Location -Path "internet_radio/Internet Radio"
-python radio.py
