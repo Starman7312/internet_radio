c81107a68cdb0a5cfbeb29fdd5d318b1b07895ca
Henry G <henrygillett@duck.com>
2025-03-26 15:35:50 +0000

    Powershell auto-installer

---
+++ Automatic Installers/windows_installer.ps1
@@ -0,0 +1,9 @@
+# Install python-vlc
+pip install python-vlc
+
+# Clone the repository
+git clone https://github.com/Starman7312/internet_radio
+
+# Navigate to the folder and run the Python script
+Set-Location -Path "internet_radio/Internet Radio"
+python radio.py
