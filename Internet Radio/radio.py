e62648951c633aace52686193aaf0e9e84b5d104
Henry G <henrygillett@duck.com>
2025-03-26 05:20:00 +0000

    Update radio.py

--- Internet Radio/radio.py
+++ Internet Radio/radio.py
@@ -4,6 +4,9 @@ from stations import stations
 
 class radio:
     def __init__(self):
+        """Instantiates object with a list of known stations
+        and creates VLC media player instance
+        """
         radio_stations = stations()
         self._station_names = []
 
@@ -14,37 +17,59 @@ class radio:
         self._stations = radio_stations.get  # Reads in all station details
         self._target = ''
         self._station = ''
+        self._volume = 50
 
         self._vlc_instance = vlc.Instance('-q')
         self._vlc_player = self._vlc_instance.media_player_new()
 
     @property
-    def tune(self, target: str):
-        """Prepares for tune to target URL and stops current playback
-
-        Args:
-            target: URL of radio station stream
+    def tune(self):
+        print('a')
+        """Prepares for tune to new station and stops current playback
         """
-        self._target = target
+        self._station = ''
         self.stop
 
     @property
     def play(self):
+        """Resumes live playback on a paused connection at current time
+        """
         self._vlc_player.play()
 
     @property
     def pause(self):
+        """Pauses connection playback, but doesn't preserve timestamp
+        e.g will resume live, not when paused
+        """
         self._vlc_player.pause()
 
     @property
     def stop(self):
+        """Completely stops playback and streaming of data
+        """
+        print('Stopping playback')
         self._vlc_player.stop()
 
     @property
     def connect(self):
+        """Connects to radio stream URL and starts playback
+        """
         media = self._vlc_instance.media_new(self._target)
         self._vlc_player.set_media(media)
         self._vlc_player.play()
+
+    @property
+    def get_volume(self):
+        return self._volume
+
+    @property
+    def available_stations(self):
+        """Lists available stations
+        """
+        num = 1
+        for s in self._station_names:
+            print('   ' + str(num) + '. ' + s)
+            num += 1
 
     def set_volume(self, value: int):
         """Sets playback volume
@@ -57,16 +82,44 @@ class radio:
         elif value < 0:
             value = 0
 
+        self._volume = value
+
         self._vlc_player.audio_set_volume(value)
         self.connect
-        return ('\nRadio volume set to: ' + str(value) + '%\n')
+        return ('Radio volume set to: ' + str(value) + '%')
+
+    @property
+    def fine_tune(self):
+        """Allows for manual selection of playback URL
+        """
+        print('Current playback via: ' + self._stat_type)
+        print('\nAvailable connections for ' + self._station + ' are:')
+
+        pos = 0
+        while pos < len(self._stations[self._station]):
+            self._stat_type, self._target = self._stations[self._station][pos]
+            print('   ' + str(pos+1) + '. ' + self._stat_type)
+            pos += 1
+
+        opt = 0
+        while (opt < 1) or (opt > pos):
+            opt = int(input('\nSelection: '))
+
+        self._stat_type, self._target = self._stations[self._station][opt-1]
+
+        print()
+        self.stop
+        self.connect
+
+        print('\nNow playing ' + self._station +
+              ', via ' + self._stat_type)
 
     @property
     def attempt_connect(self):
         """Attempts to connect to radio station via known URL's
 
         Raises:
-            ConnectionError: Raised if current URL doesn't give working playback
+            ConnectionError: Raised if current URL doesn't give working playback (based on user response)
         """
         pos = 0
         # Sets no. of connection methods
@@ -75,10 +128,10 @@ class radio:
         while pos < max:
             try:
                 # Sets station type and target URL based off name and pos
-                stat_type, self._target = self._stations[self._station][pos]
+                self._stat_type, self._target = self._stations[self._station][pos]
 
                 print('Attempting to stream ' +
-                      self._station + ', via ' + stat_type)
+                      self._station + ', via ' + self._stat_type)
 
                 self.connect
 
@@ -90,7 +143,6 @@ class radio:
                     self.stop
                     raise ConnectionError
                 else:
-                    print()
                     break
 
             except ConnectionError:
@@ -103,7 +155,7 @@ class radio:
 
         if playing.lower() in ['yes', 'y']:
             print('\nNow playing ' + self._station +
-                  ', via ' + stat_type + '\n')
+                  ', via ' + self._stat_type)
 
     @property
     def interface(self):
@@ -112,13 +164,14 @@ class radio:
         Returns:
             Boolean: True if radio should be tuned again
         """
-        # Initialises volume to 100
-        self.set_volume(100)
+        # Initialises volume to 50
+        self.set_volume(self._volume)
 
         self.attempt_connect
         try:
             command = ''
-            commands = ['tune', 'stop', 'resume', 'volume', 'exit']
+            commands = ['tune', 'fine tune',
+                        'stop', 'resume', 'volume', 'exit']
 
             while command.lower() != 'exit':
                 command = ''
@@ -128,20 +181,25 @@ class radio:
                     print('• ' + c)
 
                 while command.lower() not in commands:
-                    command = input('\ncommand: ')
+                    command = input('\nCommand: ')
 
                 if command == 'stop':
                     self.pause
                 elif command == 'resume':
                     self.play
                 elif command == 'volume':
+                    print('\nCurrent volume: ' + str(self.get_volume) + '%')
                     inp = int(input('Please enter volume (0-100): '))
-                    print(self.set_volume(inp))
+                    print('\n' + self.set_volume(inp))
                 elif command == 'tune':
                     print("\nTerminating radio stream\n")
                     self.stop
+                    print()
                     self._station = ''
                     return True
+                elif command == 'fine tune':
+                    print()
+                    self.fine_tune
 
             print("\nTerminating radio stream")
             self.stop
@@ -152,18 +210,13 @@ class radio:
             self.stop
 
     def main(self):
-        """Allow for selection of radio station via tune until the user chooses to exit
+        """Shell interface that allows for selection of radio station via tune until the user chooses to exit
         """
         run = True
         while run:
             print('Available stations: ')
+            self.available_stations
 
-            num = 1
-            for s in self._station_names:
-                print('   ' + str(num) + '. ' + s)
-                num += 1
-
-            print()
             while (self._station not in self._station_names):
                 if self._station.isnumeric():
                     self._station = int(self._station)
