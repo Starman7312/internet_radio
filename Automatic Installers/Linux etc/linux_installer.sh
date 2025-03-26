98e8ae98aa8ada1d286092b40a4777983b481904
Henry G <henrygillett@duck.com>
2025-03-26 17:18:53 +0000

    Update linux_installer.sh

--- Automatic Installers/Linux etc/linux_installer.sh
+++ Automatic Installers/Linux etc/linux_installer.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 echo "Installing python-vlc..."
 sudo apt-get install python3-vlc
 
 echo "Cloning repository..."
 git clone https://github.com/Starman7312/internet_radio
 
-echo "Navigating and running the script..."
-cd "internet_radio/Internet Radio"
+echo "Navigating to the repository directory..."
+cd "$(dirname "$0")/internet_radio/Internet Radio"
+
+echo "Running the Python script..."
 python3 radio.py
